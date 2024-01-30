d2f9ba5f89b263dff313a98e2205a24c4942a1ee
yuta0108 <urapabiron@gmail.com>
2024-01-30 19:08:15 +0900

    SecondCommit

---
+++ Sample2/src/Practice/Package.java
@@ -0,0 +1,12 @@
+package Practice;
+
+public class Package {
+	public static void main(String[] args) {
+	//  数値の表示
+		System.out.print(123);
+		System.out.print(456);
+	//  文字列の表示
+		System.out.print("ABC");
+		System.out.print("DEF");
+	}
+}

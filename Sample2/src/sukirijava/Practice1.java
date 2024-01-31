3486cbe782cdb20af1488abdc15bee7238212ec0
yuta0108 <urapabiron@gmail.com>
2024-01-31 22:21:52 +0900

    1章練習問題

---
+++ Sample2/src/sukirijava/Practice1.java
@@ -0,0 +1,14 @@
+package sukirijava;
+
+public class Practice1 {
+
+	public static void main(String[] args) {
+		// TODO 自動生成されたメソッド・スタブ
+		int a = 3;
+		int b = 5;
+		int c = a * b;
+		System.out.println("縦幅" + a + "横幅" + b + "の長方形の面積は、" + c);
+
+	}
+
+}

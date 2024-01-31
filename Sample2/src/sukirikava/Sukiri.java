764b95e8561686de5f2acfeb3e8c51f21d58b092
yuta0108 <urapabiron@gmail.com>
2024-01-31 15:53:58 +0900

    3回目のコミット

---
+++ Sample2/src/sukirikava/Sukiri.java
@@ -0,0 +1,11 @@
+package sukirikava;
+
+public class Sukiri {
+	public static void main(String[] aargs) {
+		System.out.println("菅原湊です");
+		System.out.println("22歳です");
+		System.out.println("炭酸が好きです");
+		System.out.println("22*22の計算をします");
+		System.out.println(22*22);
+	}
+}

b55c9de65db3c3b2719330310f09f8e7ec191fa5
yuta0108 <urapabiron@gmail.com>
2024-02-01 14:53:33 +0900

    2-1章オペランド

---
+++ Sample2/src/doko/Doka2.java
@@ -0,0 +1,13 @@
+package doko;
+
+public class Doka2 {
+
+	public static void main(String[] args) {
+		// TODO 自動生成されたメソッド・スタブ
+		System.out.println("私の好きな記号は二重引用符(\")です");
+		System.out.println("今日のお会計は\\2500でした。");
+		
+
+	}
+
+}
---
+++ Sample2/src/doko/Doko.java
@@ -0,0 +1,17 @@
+package doko;
+
+public class Doko {
+
+	public static void main(String[] args) {
+		// TODO 自動生成されたメソッド・スタブ
+		int a;
+		int b;
+		
+		a = 20;
+		b = a + 5;
+		System.out.println(a);
+		System.out.println(b);
+
+	}
+
+}

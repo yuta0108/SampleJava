8e516b20721ba6efb51306fcda5b321dcb4a80c1
yuta0108 <urapabiron@gmail.com>
2024-02-02 16:49:34 +0900

    2-5章終了

---
+++ Sample2/src/doko/Doko5_1.java
@@ -0,0 +1,17 @@
+package doko;
+
+public class Doko5_1 {
+
+	public static void main(String[] args) {
+		// TODO 自動生成されたメソッド・スタブ
+		double d = 8.5 / 2;
+		long l = 5 + 2L;
+		System.out.println(d);
+		System.out.println(l);
+		
+		String msg = "私の年齢は" + 24;
+		System.out.println(msg);
+
+	}
+
+}

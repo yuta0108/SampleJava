732e2397861a8df401ed3e4fc7ff347f378f53f4
yuta0108 <urapabiron@gmail.com>
2024-02-04 18:27:55 +0900

    3_4章終了

---
+++ Sample2/src/Sukirijava3/Sukiri3_4.java
@@ -0,0 +1,21 @@
+package Sukirijava3;
+
+public class Sukiri3_4 {
+
+	public static void main(String[] args) {
+		// TODO 自動生成されたメソッド・スタブ
+		System.out.println("あなたの運勢を占います");
+		int fortune = new java.util.Random().nextInt(4) + 1;
+		if (fortune == 1) {
+			System.out.println("大吉");
+		} else if (fortune == 2) {
+			System.out.println("中吉");
+		} else if (fortune == 3) {
+			System.out.println("吉");
+		} else {
+			System.out.println("凶");
+		}
+
+	}
+
+}

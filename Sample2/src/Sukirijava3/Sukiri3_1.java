e1b006a1929bd0c580f946fba2a8360fabd421aa
yuta0108 <urapabiron@gmail.com>
2024-02-04 18:06:33 +0900

    3-2章まで

---
+++ Sample2/src/Sukirijava3/Sukiri3_1.java
@@ -0,0 +1,18 @@
+package Sukirijava3;
+
+public class Sukiri3_1 {
+
+	public static void main(String[] args) {
+		// TODO 自動生成されたメソッド・スタブ
+		boolean tenki = false;
+		if (tenki == true) {
+		System.out.println("洗濯をします");
+		System.out.println("散歩に行きます");
+		} else {
+		System.out.println("漫画を読みます");
+		System.out.println("映画を見ます");
+		}
+
+	}
+
+}
---
+++ Sample2/src/Sukirijava3/Sukiri3_2.java
@@ -0,0 +1,15 @@
+package Sukirijava3;
+
+public class Sukiri3_2 {
+
+	public static void main(String[] args) {
+		// TODO 自動生成されたメソッド・スタブ
+		boolean doorClose = false; 
+		while (doorClose == true) {
+			System.out.println("ノックする");
+			System.out.println("1分待つ");
+		}
+
+	}
+
+}

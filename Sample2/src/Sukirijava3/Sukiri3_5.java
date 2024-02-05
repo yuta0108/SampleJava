fb7dbdda5f8bb18459cb0e3de12a4866f4c4303c
yuta0108 <urapabiron@gmail.com>
2024-02-05 22:44:13 +0900

    3-5章終了

---
+++ Sample2/src/Sukirijava3/Sukiri3_5.java
@@ -0,0 +1,16 @@
+package Sukirijava3;
+
+public class Sukiri3_5 {
+
+	public static void main(String[] args) {
+		// TODO 自動生成されたメソッド・スタブ
+		for (int i = 0; i < 10; i++) {
+			System.out.println("こんにちは！");
+		}
+		for (int i = 0; i < 3; i++) {
+			System.out.print("現在" + (i + 1) + "周目→");
+		}
+
+	}
+
+}

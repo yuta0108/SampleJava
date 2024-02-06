d3a05fac820f97220d3582e761f2f7795371a8da
yuta0108 <urapabiron@gmail.com>
2024-02-06 18:38:41 +0900

    4_2章終了

---
+++ Sample2/src/sukirijava4/Sukiri4_2.java
@@ -0,0 +1,17 @@
+package sukirijava4;
+
+public class Sukiri4_2 {
+
+	public static void main(String[] args) {
+		// TODO 自動生成されたメソッド・スタブ
+		int[] scores;
+		scores = new int[5];
+		scores[1] = 30;
+		System.out.println(scores[1]);
+		
+		int[] score = new int[5];
+		System.out.println(score[0]);
+
+	}
+
+}

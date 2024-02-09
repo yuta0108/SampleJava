3f98a0425aeb92e87c5c19d6347b3713793e63c9
yuta0108 <urapabiron@gmail.com>
2024-02-09 16:31:22 +0900

    4-5,6章終了

---
+++ Sample2/src/sukirijava4/Sukiri4_5.java
@@ -0,0 +1,15 @@
+package sukirijava4;
+
+public class Sukiri4_5 {
+
+	public static void main(String[] args) {
+		// TODO 自動生成されたメソッド・スタブ
+		int[] arrayA = {1, 2, 3};
+		int[] arrayB;
+		arrayB = arrayA;
+		arrayB[0] = 100;
+		System.out.println(arrayA[0]);
+
+	}
+
+}
---
+++ Sample2/src/sukirijava4/Sukiri4_6.java
@@ -0,0 +1,17 @@
+package sukirijava4;
+
+public class Sukiri4_6 {
+
+	public static void main(String[] args) {
+		// TODO 自動生成されたメソッド・スタブ
+		int[] array = {1, 2, 3};
+		array[0] = 10;
+		array = null;
+		
+//		String str = "javaで開発中";
+//		System.out.println(str.length());
+//      文字列の場合()が必要
+
+	}
+
+}

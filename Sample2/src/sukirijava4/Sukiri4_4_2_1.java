f6e3e5a2152209da9c20c0b34e05f408d12e5203
yuta0108 <urapabiron@gmail.com>
2024-02-09 15:20:58 +0900

    4-4章終了

--- Sample2/src/sukirijava4/Sukiri4_4_2_1.java
+++ Sample2/src/sukirijava4/Sukiri4_4_2_1.java
@@ -13,6 +13,15 @@ public class Sukiri4_4_2_1 {
 		for (int i = 0; i < seq.length; i++) {
 			char[] base = {'A', 'T', 'G', 'C'};
 			System.out.print(base[seq[i]] + "");
+			// int baseType = seq[i];  i番目の数値を取得
+			//char baseChar = base[baseType];　数値に対応する記号を取得
+			//System.out.print(baseChar + " ");　記号を画面に表示
+		}
+		
+		int[] scores = {20, 30, 40, 50, 80};
+		for (int value : scores) {
+			//value  価値
+			System.out.println(value);
 		}
 
 	}

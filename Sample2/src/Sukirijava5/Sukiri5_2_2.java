777ad0cb7122ad2c8c8a6212f2285522d0aaf0a6
yuta0108 <urapabiron@gmail.com>
2024-02-12 17:19:02 +0900

    [finish]

---
+++ Sample2/src/Sukirijava5/Sukiri5_2_2.java
@@ -0,0 +1,16 @@
+package Sukirijava5;
+
+public class Sukiri5_2_2 {
+
+	public static void main(String[] args) {
+		// TODO 自動生成されたメソッド・スタブ
+		add(100, 20);
+		add(200, 50);
+	}
+	//複数の値を受け取るaddメソッド
+	public static void add(int x, int y) {
+		int ans = x + y;
+		System.out.println(x + "+" + y + "=" + ans);
+	}
+
+}
---
+++ Sample2/src/Sukirijava5/Sukiri5_3.java
@@ -0,0 +1,10 @@
+package Sukirijava5;
+
+public class Sukiri5_3 {
+
+	public static void main(String[] args) {
+		// TODO 自動生成されたメソッド・スタブ
+
+	}
+
+}

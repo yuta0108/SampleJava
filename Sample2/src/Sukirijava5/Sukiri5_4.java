1996e24607f9b4c068732fa8074661d3518c7fee
yuta0108 <urapabiron@gmail.com>
2024-02-12 23:52:22 +0900

    [finish]p197

---
+++ Sample2/src/Sukirijava5/Sukiri5_4.java
@@ -0,0 +1,21 @@
+package Sukirijava5;
+
+public class Sukiri5_4 {
+	public static int add(int x, int y) {
+		return x + y;
+	}
+	public static double add(double x, double y) {
+		return x + y;
+	}
+	public static String add(String x, String y) {
+		return x + y;
+	}
+
+	public static void main(String[] args) {
+		System.out.println(add(10, 20));
+		System.out.println(add(3.5, 2.7));
+		System.out.println(add("Hello", "World"));
+	}
+	
+
+}

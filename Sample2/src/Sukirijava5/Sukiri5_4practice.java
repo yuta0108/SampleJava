aeb9611d09771a63cbce066b348b33ba0d221c37
yuta0108 <urapabiron@gmail.com>
2024-02-19 17:03:28 +0900

    5章練習問題終了

---
+++ Sample2/src/Sukirijava5/Sukiri5_4practice.java
@@ -0,0 +1,16 @@
+package Sukirijava5;
+
+public class Sukiri5_4practice {
+	public static double calcTriangleArea(double x, double y) {
+		return x * y / 2;
+	}
+	public static double calcCircleArea(double x) {
+		return x * x * 3.14;
+	}
+
+	public static void main(String[] args) {
+		System.out.println(calcTriangleArea(10, 5));
+		System.out.println(calcCircleArea(5));
+	}
+
+}

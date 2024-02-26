41cf42bf8e1301965e4330bd0c2632c98b13b546
yuta0108 <urapabiron@gmail.com>
2024-02-26 16:53:02 +0900

    [finish]

---
+++ Sample2/src/sukiri9practice/Main.java
@@ -0,0 +1,19 @@
+package sukiri9practice;
+
+public class Main {
+
+	public static void heal(int hp) {
+		hp += 10;
+	}
+	public static void heal(Thief thief) {
+		thief.hp += 10;
+	}
+	public static void main(String[] args) {
+		int baseHp = 25;
+		Thief t = new Thief("アサカ", baseHp);
+		heal(baseHp);
+		System.out.println(baseHp + ":" + t.hp + ":" + t.mp );
+		heal(t);
+		System.out.println(baseHp + ":" + t.hp );
+	}
+}

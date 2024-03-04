4b1fe69426c10590d4ffe902387fb372857aac7d
yuta0108 <urapabiron@gmail.com>
2024-03-04 17:15:30 +0900

    p403

--- Sample2/src/Sukiri11/Character.java
+++ Sample2/src/Sukiri11/Character.java
@@ -1,13 +1,16 @@
 package Sukiri11;
 
-public class Character {
+public abstract class Character {
+	//抽象クラスとしてCharacterを宣言
 	String name;
 	int hp;
 	public void run() {
 		System.out.println(this.name + "は、逃げ出した！");
 	}
-	public void attack(Matango m) {
+	public abstract void attack(Matango m);
 		//attackメソッドの内容決めていないため一端空にする
 		//必ずオーバーライドして使うこと！
-	}
+		//abstract: 抽象的 抽象メソッド
+		//内容がどうなるかまでは現時点では確定できないので
+		//(メソッド内部の処理はここでは記載しません)という意味
 }

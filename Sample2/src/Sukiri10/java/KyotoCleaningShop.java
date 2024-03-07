2c9027aaaec5387440e2fe5dfe37161b02b9d5d0
yuta0108 <urapabiron@gmail.com>
2024-03-07 14:42:56 +0900

    インターフェイスについて

--- Sample2/src/Sukiri10/java/KyotoCleaningShop.java
+++ Sample2/src/Sukiri10/java/KyotoCleaningShop.java
@@ -1,5 +1,22 @@
 package Sukiri10.java;
-
-public class KyotoCleaningShop {
-
+//インターフェイスを継承して子クラスを定義する場合：implementsを使用
+public class KyotoCleaningShop implements
+	CleaningService {
+	String ownerName;
+	String address;
+	String phone;
+	//シャツを洗う
+	public Shirt washShirt(Shirt s) {
+		//大型洗濯機15分
+		return s;
+	}
+	//タオルを洗う
+	public Towl washTowl(Towl t) {
+		//大型洗濯機10分
+		return t;
+	}
+	public Coat washCoat(Coat c) {
+		//ドライ20分
+		return c;
+	}
 }

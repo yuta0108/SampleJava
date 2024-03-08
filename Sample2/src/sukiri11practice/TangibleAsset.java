e67f403b12878a748446988d3ddccc590bbf68ef
yuta0108 <urapabiron@gmail.com>
2024-03-08 16:00:17 +0900

    11章練習問題終了

--- Sample2/src/sukiri11practice/TangibleAsset.java
+++ Sample2/src/sukiri11practice/TangibleAsset.java
@@ -1,15 +1,16 @@
 package sukiri11practice;
 
-public abstract class TangibleAsset {
-	String name;
-	int price;
+public abstract class TangibleAsset extends Asset implements Thing {
 	String color;
-	public TangibleAsset(String name, int price, String color) {
-		this.name = name;
-		this.price = price;
+	double weight;
+	
+	public TangibleAsset(String name, int price, String color, double weight) {
+		super(name, price);
 		this.color = color;
+		this.weight = weight;
+		
 	}
-	public String getName()  {return this.name; }
-	public int getPrice() {return this.price;}
 	public String getColor() {return this.color;}
+	public double getWeight() {return this.weight;}
+	public void setWeight(double weight) {this.weight = weight;}
 }
--- Sample2/src/sukiri11practice/Thing.java
+++ Sample2/src/sukiri11practice/Thing.java
@@ -2,5 +2,5 @@ package sukiri11practice;
 
 public interface Thing {
 	double getWeight(); //重さを取得するメソッド
-	void setWeight(double weight); //重さを設定するメソッド
+	double setWeight(); //重さを設定するメソッド
 }

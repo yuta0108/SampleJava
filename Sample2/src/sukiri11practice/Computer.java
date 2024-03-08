28a55dcd32f5777758cd37f35bc1b6514fab609f
yuta0108 <urapabiron@gmail.com>
2024-03-08 15:25:51 +0900

    11-1練習問題

---
+++ Sample2/src/sukiri11practice/Computer.java
@@ -0,0 +1,10 @@
+package sukiri11practice;
+
+public class Computer extends TangibleAsset {
+	String makerName;
+	public Computer(String name, int price, String color,String makerName) {
+		super(name,price,color);
+		this.makerName = makerName;
+	}
+	public String getMakerName() {return this.makerName;}
+}

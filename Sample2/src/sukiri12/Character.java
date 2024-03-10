959a51f12630b52e0e5faf04825db726e00bf6d1
yuta0108 <urapabiron@gmail.com>
2024-03-10 16:16:08 +0900

    [finish]

---
+++ Sample2/src/sukiri12/Character.java
@@ -0,0 +1,8 @@
+package sukiri12;
+
+public abstract class Character {
+	String name;
+	int hp;
+	public void run() {}
+	public abstract void attack(Matango m);
+}
--- Sample2/src/sukiri12/Wizard.java
+++ Sample2/src/sukiri12/Wizard.java
@@ -1,5 +1,16 @@
 package sukiri12;
 
-public class Wizard {
-
+public class Wizard extends Character {
+	int mp;
+	public void attack(Matango m) {
+		System.out.println(this.name + "の攻撃！");
+		System.out.println("敵に3ポイントのダメージ！");
+		m.hp -= 3;
+	}
+	public void fireball(Matango m) {
+		System.out.println(this.name + "は火の玉を放った！");
+		System.out.println("敵に20のダメージ！");
+		m.hp -= 20;
+		this.mp -= 5;
+	}
 }

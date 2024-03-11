892cb6b5f400c0803b14ea39311ea316001d60fe
yuta0108 <urapabiron@gmail.com>
2024-03-11 16:44:25 +0900

    [finish]

--- Sample2/src/sukiri12/Character.java
+++ Sample2/src/sukiri12/Character.java
@@ -4,5 +4,5 @@ public abstract class Character {
 	String name;
 	int hp;
 	public void run() {}
-	public abstract void attack(Matango m);
+	public abstract void attack(Monster m);
 }
--- Sample2/src/sukiri12/Hero.java
+++ Sample2/src/sukiri12/Hero.java
@@ -1,14 +1,20 @@
 package sukiri12;
 
 public class Hero extends Character {
-	public void attack (Matango m) {
+//	public void attack (Matango m) {
+//		System.out.println(this.name + "の攻撃！");
+//		System.out.println("敵に5のダメージ！");
+//		m.hp -= 5;
+//	}
+//	public void attack(Slime s) {
+//		System.out.println(this.name + "の攻撃！");
+//		System.out.println("敵に5のダメージ！");
+//		s.hp -= 5;
+//	}
+	//Monsterなら何でも攻撃できるattackメソッド
+	public void attack(Monster m) {
 		System.out.println(this.name + "の攻撃！");
 		System.out.println("敵に5のダメージ！");
 		m.hp -= 5;
-	}
-	public void attack(Slime s) {
-		System.out.println(this.name + "の攻撃！");
-		System.out.println("敵に5のダメージ！");
-		s.hp -= 5;
 	}
 }

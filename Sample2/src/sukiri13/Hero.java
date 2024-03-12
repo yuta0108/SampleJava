b9f4425fcb5f4842cfc5cec4a327832b3b430d34
yuta0108 <urapabiron@gmail.com>
2024-03-12 16:35:24 +0900

    p475アクセス制限

--- Sample2/src/sukiri13/Hero.java
+++ Sample2/src/sukiri13/Hero.java
@@ -1,11 +1,18 @@
 package sukiri13;
 
 public class Hero {
+	//他のクラスから直接変更できる必要がないためprivateを使用
 	private int hp;
 	String name;
 	Sword sword;
 	public void sleep() {
 		this.hp = 100;
 		System.out.println(this.name + "は、眠って回復した！");
+	}
+	
+	//privateによりdieメソッドは外部のメソッドから呼び出せない
+	private void die() {
+		System.out.println(this.name + "は死んでしまった！");
+		System.out.println("GAME OVERです。");
 	}
 }
